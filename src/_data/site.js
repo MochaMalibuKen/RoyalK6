a31cff721746163ebee37f3a7630ab534b2187ae
MochaMalibuKen <your_email@example.com>
2026-01-05 15:26:52 -0600

    Add site lock overlay and enable hold screen

---
+++ src/_data/site.js
@@ -0,0 +1,7 @@
+module.exports = {
+  locked: true,
+  lockHeading: "We'll be right back.",
+  lockMessage: "This site is temporarily unavailable. Please check back soon.",
+  contactEmail: "info@royalk6.org",
+  contactPhone: "706-329-2557"
+};
